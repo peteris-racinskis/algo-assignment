fce4c8f167bfffa5c1270458f7b5a25bd2457619
Your Name <peterisracinskis@gmail.com>
2021-01-18 00:49:30 +0200

    lmao it actually works

--- main.py
+++ main.py
@@ -1,4 +1,52 @@
 #!/usr/bin/python3
+import sys
+import re
+from structures import DjSet as st
 
 
-print("test")
+def process_graph(data):
+    vertex_count, edges = get_edges(data)
+    result = get_non_positive(edges)
+    remaining = get_positive(edges)
+    forest = st([])
+    for w,u,v in remaining:
+        if not forest.same_subset(u,v):
+            forest.union(u,v)
+        else:
+            result.append((w,u,v,))
+    print(result)
+    print("COST : {}".format(sum(int(w) for w,u,v in result)))
+    [print(x) for x in result]
+    print(remaining)
+
+def get_positive(edges):
+    return list(filter(lambda x: x[0] > 0, edges))
+
+def get_non_positive(edges):
+    return list(filter(lambda x: x[0] <= 0, edges))
+
+def get_edges(data):
+    vertex_count = data[0]
+    data = data[1:]
+    edge_number = float(len(data)-1)/3.0
+    edges = []
+    while len(data) > 0:
+        edges.append((
+            int(data.pop()),
+            int(data.pop()),
+            int(data.pop()),
+        ))
+    edges.sort(key = lambda x: -x[0])
+    return vertex_count, edges
+
+def parse_input(path):
+    with open(path, 'r') as f:
+        d = f.read()
+    return list(filter(lambda x: x != "", re.split("\s{1,}", d)))
+
+args = sys.argv
+if len(args) > 1:
+    filenames = args[1:]
+for f in filenames:
+    ret = parse_input(f)
+    process_graph(ret)
---
+++ structures.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+class DjNode:
+
+    def __init__(self, x):
+        self.x = x
+        self.parent = None
+        self.size = 1
+    
+    def __repr__(self):
+        return "Node({}, {}, {})".format(self.x, self.parent, self.size)
+
+    def get_root(self):
+        if self.parent:
+            root = self.parent.get_root()
+            self.update_parent(root)
+            return root
+        return self
+
+    def update_parent(self, y):
+        self.parent = y
+    
+    def get_size(self):
+        return self.size
+
+    def add_size(self, x):
+        self.size = self.size + x
+
+
+"""
+
+Methods:
+    
+    find_root(x): 
+    - returns x if parent(x) is x
+    - returns find_root(parent(x)) if x is not own parent
+    - updates parent(x) to find_root(x)
+    
+    union(x,y):
+    - checks if find_root same for x, y
+    - if not, compare number of children for each root
+    - parent(smaller) <- bigger
+    - children(bigger) = children(smaller) + children(bigger)
+
+"""
+
+
+class DjSet:
+
+    def __init__(self, iterable):
+        self._nodes = {x:DjNode(x) for x in iterable}
+
+    def __repr__(self):
+        return "Disjoint Set: {} ".format(self._nodes)
+        
+    def find_root(self, x):
+        if x not in self._nodes:
+            self._nodes[x] = DjNode(x)
+        return self._nodes[x].get_root()
+
+    def same_subset(self, x, y):
+        return self.find_root(x).x == self.find_root(y).x
+
+    def union(self, x, y):
+        if self.same_subset(x,y):
+            return
+        roots = [self._nodes[x].get_root(), self._nodes[y].get_root()]
+        roots.sort(key = lambda x: x.get_size())
+        roots[0].update_parent(roots[1])
+        roots[1].add_size(roots[0].get_size())
+        
+
+
+
+def do_work(edges):
+    p = get_positive(edges)
+    discarded = get_non_positive(edges)
+    vertices = set([x[1] for x in p])
+    vertices.add(set([x[2] for x in p]))
+    trees = DjSet(vertices)
+    for w,u,v in p:
+        if not trees.same_subset(u,v):
+            trees.union(u,v)
+        else:
+            discarded.append((w,u,v,))
+    
+        
+
+
+
